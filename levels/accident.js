0fe8c798d87431fb0c46fa153b37a5f16cbe6185
Osthekake <osthekake@gmail.com>
2015-08-24 22:54:42 +0200

    image loading. swapped images.

--- levels/accident.js
+++ levels/accident.js
@@ -46,11 +46,14 @@ sceneData["accident1_outro"] = [
 
 sceneData["accident2"] = [ 
 	{
+		application: loadingFunctions.setCrack,
+		level: 2
+	},{
 		application: loadingFunctions.text,
 		text: "I was nearest to the gas extraction platform when the alarm went off, and I was the first on the scene. A leakage in the gas transferral system. If I couldn't stop the leak quickly, it would grow to a full rupture, and we would lose the tanks and maybe the whole spoke-arm of the orbital station." 
 	},{
 		application: loadingFunctions.picture,
-		path: "next.png" 
+		path: "spaceship_interior.jpg" 
 	},{
 		application: loadingFunctions.text,
 		text: "I shut the airlock and started cycling the air out of the room I was in. I had to do it carefully or I wouldn't be able to find the leak before the pressure changes tore the metal apart, but I would never be able to find it in time otherwise."
