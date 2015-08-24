8456f186a8465644a9525f532ce121dcff793dce
Osthekake <osthekake@gmail.com>
2015-08-25 01:30:38 +0200

    fixed broken links. fixed crack

--- levels/birthday.js
+++ levels/birthday.js
@@ -72,7 +72,7 @@ sceneData["Birthday_1_3"] = [
 		choices: [ 
 		  {
 		  	text: "I will always remember the party and blowing out all the candles in one go. and being chased around by Rosie's dog.",
-		  	goal: "tracking1"  
+		  	goal: "tracking_1"  
 		  }
 		] 
 	}
@@ -103,7 +103,6 @@ sceneData["Birthday_2"] = [
 		] 
 	}
 ];
-
 
 sceneData["Birthday_2_1"] = [ 
 	{
